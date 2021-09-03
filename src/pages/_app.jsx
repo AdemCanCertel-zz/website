bf07810a55bc0945e77a2e050022e303a6872396
AdemCan <69676558+AdemCanCertel@users.noreply.github.com>
2021-09-03 12:57:08 +0300

    inital

---
+++ src/pages/_app.jsx
@@ -0,0 +1,6 @@
+import 'tailwindcss/tailwind.css'
+
+function myApp({ Component, pageProps }) {
+    return <Component {...pageProps} />
+}
+export default myApp
---
+++ src/pages/index.jsx
@@ -0,0 +1,4 @@
+function HomePage() {
+    return <div>Hello, world!</div>
+}
+export default HomePage
