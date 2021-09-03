6c43bccfc8642bcfc9f3fce8b4b48ee3a085fbcb
AdemCan <69676558+AdemCanCertel@users.noreply.github.com>
2021-09-03 21:42:44 +0300

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,15 +5,16 @@ export default function Header() {
         <header className="full-container py-10">
             <nav className="space-x-5">
                 <Link href="/">
-                    <a className="hover:bg-gray-400 p-4	px-4 py-4 rounded-lg">Home</a>
+                    <a className="hover:bg-gray-400 p-4 px-4 py-4 rounded-lg">Home</a>
                 </Link>
                 <Link href="/blog">
-                    <a className="hover:bg-gray-400 p-4	px-4 py-4 rounded-lg">Blog</a>
+                    <a className="hover:bg-gray-400b p-4 px-4 py-4 rounded-lg">Blog</a>
                 </Link>
                 <Link href="/about">
-                    <a className="hover:bg-gray-400 p-4	px-4 py-4 rounded-lg">About</a>
+                    <a className="hover:bg-gray-400 p-4  px-4 py-4 rounded-lg">About</a>
                 </Link>
             </nav>
+            <hr className="mt-4 opacity-30" />
         </header>
     )
 }
