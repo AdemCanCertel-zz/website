1dc02677bc5029cf11402f7d49e9ec51784c2649
AdemCan <69676558+AdemCanCertel@users.noreply.github.com>
2021-09-03 17:34:36 +0300

    update

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,11 +4,11 @@ export default function HomePage() {
     return  (
         <div className="max-w-2xl mx-auto">
             <div className="space-y-4">
-            <h1 className="text-4xl font-semibold"><Link href="/">AdemCan Certel</Link></h1>
-            <p className="mt-5">
+            <h1 className="text-4xl font-extrabold text-gray-400"><Link href="/">Hi, I'm AdemCan Certel</Link></h1>
+            <p className="mt-5 font-medium">
                 Hi, I'm Front End and a high school student, 
                 I have about 5 years of experience with JavaScript. 
-                NextJS, NuxtJS, ReactJS are the technologies I use all the time. <Link className="text-indigo-500" href="">Would you like to learn more about me</Link>
+                NextJS, NuxtJS, ReactJS are the technologies I use all the time. <Link href="/">(Would you like to learn more about me)</Link>
             </p>
             </div>
         </div>
