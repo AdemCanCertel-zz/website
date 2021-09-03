f68b0b11a639c014c47da48dd74e4efafdffd154
AdemCan <69676558+AdemCanCertel@users.noreply.github.com>
2021-09-03 14:48:07 +0300

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,24 @@
+import Link from 'next/link';
+
 function HomePage() {
-    return <div className="text-green-400">Hello, world!</div>
+    return <main>
+            <header>
+            <div className="flex items-center">
+                <Link href="/">
+                    <h1 className="font-semibold pl-3 mr-1 text-2xl inline-block">AdemCan</h1>
+                </Link>
+            </div>
+        </header>
+            <section>
+                <h1>
+                    Hey, I'm AdemCan Certel👋
+                </h1>
+                <p className="prose">
+                    Hi, I'm Front End and a high school student, 
+                    I have about 5 years of experience with JavaScript. 
+                    NextJS, NuxtJS, ReactJS are the technologies I use all the time. <Link className="text-indigo-500" href="">Would you like to learn more about me</Link>
+                </p>
+            </section>
+        </main>
 } 
 export default HomePage
