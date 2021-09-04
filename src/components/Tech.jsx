64e6962794ca092f1d6b959c71652a8a78221e19
AdemCan <69676558+AdemCanCertel@users.noreply.github.com>
2021-09-04 21:45:44 +0300

    update

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -3,7 +3,8 @@ export default function Tech() {
         <div className="flex mt-4">
         <div>
             <h1 className="font-semibold text-3xl text-white">Technologies I use</h1>
-        </div>    
+        </div>
+            
         </div>
     )
 }
