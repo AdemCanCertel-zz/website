1d06208fc1e7b1a4f6bd2abb74fd567fd6218ee1
AdemCan <69676558+AdemCanCertel@users.noreply.github.com>
2021-09-21 21:13:05 +0300

    new typescript ✨
    new added typescript

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import '../assets/css/global.css'
-import css from '../assets/css/main.css'
 import Header from '../components/Header/Header'
 import ReactDOM from 'react-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
